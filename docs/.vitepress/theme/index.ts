d872a67e4f9cb9c6e7c615b25b2c3fc7c63282bd
Axi404 <axihelloworld@gmail.com>
2024-07-03 00:54:07 +0800

    add giscus

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,6 +3,8 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData, useRoute } from 'vitepress';
 import { 
   NolebaseEnhancedReadabilitiesMenu, 
   NolebaseEnhancedReadabilitiesPlugin,
@@ -15,6 +17,30 @@ import './style.css'
 
 export default {
   extends: DefaultTheme,
+  setup() {
+    // Get frontmatter and route
+    const { frontmatter } = useData();
+    const route = useRoute();
+        
+    // giscus配置
+    giscusTalk({
+      repo: 'XiStudyGroup/XiStudyGroup.github.io', //仓库
+      repoId: 'R_kgDOMQMxzA', //仓库ID
+      category: 'Announcements', // 讨论分类
+      categoryId: 'DIC_kwDOMQMxzM4CgiHQ', //讨论分类ID
+      mapping: 'pathname',
+      inputPosition: 'bottom',
+      lang: 'zh-CN',
+      }, 
+      {
+        frontmatter, route
+      },
+      //默认值为true，表示已启用，此参数可以忽略；
+      //如果为false，则表示未启用
+      //您可以使用“comment:true”序言在页面上单独启用它
+      true
+    );
+  },
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
